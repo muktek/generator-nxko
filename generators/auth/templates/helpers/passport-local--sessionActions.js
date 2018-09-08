8babd611402a560b32e6777fc6b8e3ea7fe48c50
tphdev <tphdevdrop@gmail.com>
2018-09-08 13:29:13 -0500

    fixing sessionActions

--- generators/auth/templates/helpers/passport-local--sessionActions.js
+++ generators/auth/templates/helpers/passport-local--sessionActions.js
@@ -7,15 +7,6 @@ exports.configSerializeUser = function( config={} ) {
   }
 }
 
-const User = require('../models/User.js')
-
-
-exports.configSerializeUser = function( config={} ) {
-  return function(user, done){
-    done(null, user.id)
-  }
-}
-
 exports.configDeserializeUser = function( config={} ) {
   return async function(userId, done){
     try {
